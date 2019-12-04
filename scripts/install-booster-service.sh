0aee35ab45dd8104137e8679be779e1edef532d8
FRCAluminati3555 <frcAluminati@gmail.com>
2019-12-04 10:19:34 -0500

    Removed booster program

--- scripts/install-booster-service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo cp ../services/booster.service /etc/systemd/system
-sudo systemctl enable booster.service
--- scripts/remove-booster-service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo systemctl disable booster.service
-sudo rm /etc/systemd/system/booster.service
